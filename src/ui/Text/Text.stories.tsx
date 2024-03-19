839f3d27ce8e143bfe4c04dbc55c99eb0a43dd20
Gabriel San Martin <gabisanmartin12@gmail.com>
2024-03-19 11:28:17 -0300

    add font size styles and improve TextVariant type.

--- src/ui/Text/Text.stories.tsx
+++ src/ui/Text/Text.stories.tsx
@@ -1,8 +1,14 @@
-import { Text } from "@/ui";
+import { Text, TextVariant } from "@/ui";
 import type { Meta, StoryObj } from "@storybook/react";
 
 const meta: Meta<typeof Text> = {
   component: Text,
+  argTypes: {
+    variant: {
+      options: TextVariant,
+      control: { type: "select" },
+    },
+  },
   title: "Ui/Text",
 };
 
@@ -11,6 +17,7 @@ type Story = StoryObj<typeof Text>;
 export const Playground: Story = {
   args: {
     children: "Some text",
+    variant: "heading1",
   },
 };
 
--- src/ui/Text/Text.tsx
+++ src/ui/Text/Text.tsx
@@ -1,32 +1,43 @@
 import { HTMLAttributes } from "react";
 
-type TextVariant =
-  | `heading${1 | 2 | 3 | 4 | 5 | 6}`
-  | `title${1 | 2}`
-  | "subtitle"
-  | "lead"
-  | `body${1 | 2 | 3}`
-  | "small";
+export const TextVariant = [
+  "body1",
+  "body2",
+  "body3",
+  "heading1",
+  "heading2",
+  "heading3",
+  "heading4",
+  "heading5",
+  "heading6",
+  "lead",
+  "small",
+  "subtitle",
+  "title1",
+  "title2",
+] as const;
 
-const STYLES_BY_VARIANT: Record<TextVariant, HTMLElement["className"]> = {
-  body1: "",
-  body2: "",
-  body3: "",
-  heading1: "",
-  heading2: "",
-  heading3: "",
-  heading4: "",
-  heading5: "",
-  heading6: "",
-  lead: "",
-  small: "",
-  subtitle: "",
-  title1: "",
-  title2: "",
+type TextVariantType = (typeof TextVariant)[number];
+
+const STYLES_BY_VARIANT: Record<TextVariantType, string> = {
+  body1: "text-lg/6",
+  body2: "text-base/5",
+  body3: "text-sm",
+  heading1: "text-5xl/[3.25rem]",
+  heading2: "text-[2.5rem]/[2.75rem]",
+  heading3: "text-[2rem]/9",
+  heading4: "text-[1.75rem]/8",
+  heading5: "text-2xl/7",
+  heading6: "text-xl/6",
+  lead: "text-xl/6",
+  small: "text-xs",
+  subtitle: "text-xs",
+  title1: "text-base/5",
+  title2: "text-sm",
 };
 
 export type TextProps = HTMLAttributes<HTMLParagraphElement> & {
-  variant: TextVariant;
+  variant: TextVariantType;
 };
 
 export const Text = ({ variant, ...props }: TextProps) => {
