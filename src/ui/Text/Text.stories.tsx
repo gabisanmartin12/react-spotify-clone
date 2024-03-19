b67f47f6f7babd77c19cb7e5963de2dfa440a6c8
Gabriel San Martin <gabisanmartin12@gmail.com>
2024-03-19 14:24:19 -0300

    add text docs.

--- src/ui/Text/Text.stories.tsx
+++ src/ui/Text/Text.stories.tsx
@@ -1,14 +1,50 @@
 import { Text, TextVariant } from "@/ui";
-import type { Meta, StoryObj } from "@storybook/react";
+import { Subtitle, Title, Typeset } from "@storybook/blocks";
+import { Meta, StoryObj } from "@storybook/react";
 
 const meta: Meta<typeof Text> = {
-  component: Text,
   argTypes: {
     variant: {
-      options: TextVariant,
       control: { type: "select" },
+      options: TextVariant,
     },
   },
+  component: Text,
+  parameters: {
+    docs: {
+      docsMode: true,
+      page: () => (
+        <>
+          <Title>Typography</Title>
+
+          <Subtitle>Circular Spotify Text</Subtitle>
+          <Typeset
+            fontWeight={600}
+            sampleText="Heading"
+            fontFamily="CircularSpotifyText"
+            fontSizes={["48px", "40px", "32px", "28px", "24px", "20px"]}
+          />
+
+          <Subtitle>Sf Pro Text</Subtitle>
+          <Typeset
+            fontWeight={400}
+            sampleText="Text"
+            fontFamily="SfProText"
+            fontSizes={["20px", "18px", "16px", "14px", "12px"]}
+          />
+
+          <Subtitle>Sf Pro Text Bold</Subtitle>
+          <Typeset
+            fontWeight={600}
+            sampleText="Text"
+            fontFamily="SfProText"
+            fontSizes={["20px", "18px", "16px", "14px", "12px"]}
+          />
+        </>
+      ),
+    },
+  },
+  tags: ["autodocs"],
   title: "Ui/Text",
 };
 
@@ -16,8 +52,8 @@ type Story = StoryObj<typeof Text>;
 
 export const Playground: Story = {
   args: {
-    children: "Some text",
-    variant: "heading1",
+    children: "Play with me",
+    variant: "body1",
   },
 };
 
